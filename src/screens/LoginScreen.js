1a9fb1c774779ae61e755dc2cd79bec6116a5456
Joe Smith <joeycrushed@gmail.com>
2021-09-02 17:19:08 +0100

    sign in button

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,8 +5,12 @@ function LoginScreen() {
     return (
         <div className="loginScreen">
             <div className="loginScreen__background">
-                <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+                <img 
+                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+                className="loginScreen__logo"
+                alt="" />
             </div>
+            <button className="loginScreen__button">Sign In</button>
         </div>
     )
 }
