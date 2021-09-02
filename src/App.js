5a1ff53dc98ca05be3fa0bc3621ddc5a0ad17027
Joe Smith <joeycrushed@gmail.com>
2021-09-02 16:51:32 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from "./HomeScreen";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <HomeScreen />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <HomeScreen />
+        </Route>
+      </Switch>
+  </Router>
+);
   );
 }
 
