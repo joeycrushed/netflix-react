9748208ebd57110e80abff52d8a1385eebf8b93e
Joe Smith <joeycrushed@gmail.com>
2021-09-06 13:11:27 +0100

    login screen text content

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -16,6 +16,11 @@ function LoginScreen() {
             <div className="loginScreen__body">
                 <>
                 <h1>Unlimited films, TV programmes and more.</h1>
+                <h2>Watch anywhere. Cancel at any time</h2>
+                <h3>Ready to watch? Enter your email to create or restart your membership</h3>
+                <div className="loginScreen__input">
+                    
+                </div>
                 </>
             </div>
         </div>
