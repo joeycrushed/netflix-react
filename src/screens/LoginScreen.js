bb38e81090ec87537a46ed0e69219b832fd0501f
Joe Smith <joeycrushed@gmail.com>
2021-09-06 13:36:37 +0100

    add state for sign up

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginScreen.css'
+import SignupScreen from './SignupScreen';
 
 function LoginScreen() {
+    const [signIn, setSignIn] = useState(false);
+
     return (
         <div className="loginScreen">
             <div className="loginScreen__background">
@@ -10,18 +13,30 @@ function LoginScreen() {
                 className="loginScreen__logo"
                 alt="" />
             </div>
-            <button className="loginScreen__button">Sign In</button>
+            <button onClick={() => setSignIn(true)}
+            className="loginScreen__button">Sign In</button>
 
             <div className="loginScreen__gradient"></div>
             <div className="loginScreen__body">
-                <>
-                <h1>Unlimited films, TV programmes and more.</h1>
-                <h2>Watch anywhere. Cancel at any time</h2>
-                <h3>Ready to watch? Enter your email to create or restart your membership</h3>
-                <div className="loginScreen__input">
-                    
-                </div>
-                </>
+                {signIn ? (
+                    <SignupScreen />
+                ) : (
+                    <>
+                    <h1>Unlimited films, TV programmes and more.</h1>
+                    <h2>Watch anywhere. Cancel at any time</h2>
+                    <h3>Ready to watch? Enter your email to create or restart your membership</h3>
+                    <div className="loginScreen__input">
+                        <form >
+                            <input type="email" 
+                            placeholder="Email Address"/>
+                            <button onClick={() => setSignIn(true)}
+                            className="loginScreen__getStarted">Get Started</button>
+    
+                        </form>
+                    </div>
+                    </>
+                )}
+            
             </div>
         </div>
     )
