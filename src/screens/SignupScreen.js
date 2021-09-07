659e19e00bbebb07fa47fed6205f83f2620242c5
Joe Smith <joeycrushed@gmail.com>
2021-09-07 19:26:18 +0100

    sign up screen styling

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -9,6 +9,10 @@ function SignupScreen() {
                 <input placeholder="Email" type="email" />
                 <input placeholder="Password" type="text" />
                 <button type="submit">Sign In</button>
+                <h4>
+                    <span className="signupScreen__gray">New to Netflix? </span>
+                    <span className="signupScreen__link">Sign Up Now</span>
+                </h4>
             </form>
         </div>
     )
