b23cd48cefdd938164af17d1ea68302335634248
Joe Smith <joeycrushed@gmail.com>
2021-09-08 16:50:01 +0100

    firebase problem resolved

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -11,11 +11,25 @@ const passwordRef = useRef(null);
     const register = (e) => {
         e.preventDefault();
 
-        
-    }
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser)
+        }).catch((error) => {
+            alert(error.message)
+        });
+    };
 
     const signIn = (e) => {
         e.preventDefault();
+
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser)
+        }).catch((error) => alert(error.message));
     }
 
 
@@ -23,12 +37,12 @@ const passwordRef = useRef(null);
         <div className="signupScreen">
             <form>
                 <h1>Sign In</h1>
-                <input placeholder="Email" type="email" />
-                <input placeholder="Password" type="text" />
-                <button type="submit">Sign In</button>
+                <input ref={emailRef} placeholder="Email" type="email" />
+                <input ref={passwordRef} placeholder="Password" type="text" />
+                <button onClick={signIn} type="submit">Sign In</button>
                 <h4>
                     <span className="signupScreen__gray">New to Netflix? </span>
-                    <span className="signupScreen__link">Sign Up Now</span>
+                    <span onClick={register} className="signupScreen__link">Sign Up Now</span>
                 </h4>
             </form>
         </div>
