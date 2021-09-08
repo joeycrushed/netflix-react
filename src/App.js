1dcf9e33c25164dd12049f1615d5601d2e2a4c7a
Joe Smith <joeycrushed@gmail.com>
2021-09-08 17:26:46 +0100

    add profile page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen'
+import { auth } from "./firebase"
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from "./features/counter/userSlice"
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout)
+      }
+    })
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
       <Router>
@@ -13,7 +33,10 @@ function App() {
           <LoginScreen />
         ) : (
         <Switch>
-          <Route path="/">
+          <Route path="/profile">
+            <ProfileScreen />
+          </Route>
+          <Route path="/">          
             <HomeScreen />
           </Route>
         </Switch>
