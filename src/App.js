8c65d7b70b3c288a7a2ef46e3c68ba6e3a7bec10
Joe Smith <joeycrushed@gmail.com>
2021-09-09 15:15:27 +0100

    profile screen

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ function App() {
           email: userAuth.email
         }))
       } else {
-        dispatch(logout)
+        dispatch(logout())
       }
     })
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="app">
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from "../features/counter/userSlice"
+import { auth } from '../firebase'
+import Nav from '../Nav.js'
 import './ProfileScreen.css'
 
 function ProfileScreen() {
+    const user = useSelector(selectUser)
+
     return (
-        <div>
-            <h1>This is the profile screen</h1>
+        <div className="profileScreen">
+            <Nav />
+            <div className="profileScreen__body">
+                <h1>Edit Profile</h1>
+                <div className="profileScreen__info">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
+                
+                    <div className="profileScreen__details">
+                        <h2>{user.email}</h2>
+                        <div className="profileScreen__plans">
+                            <h3>Plans</h3>
+                            <button onClick={() => auth.signOut()} className="profileScreen__signOut">Sign Out</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
