313963f7b5d2910333a46c5f45eef25cee58b2ff
Joe Smith <joeycrushed@gmail.com>
2021-09-09 15:39:47 +0100

    styling

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -20,6 +20,7 @@ function ProfileScreen() {
                         <h2>{user.email}</h2>
                         <div className="profileScreen__plans">
                             <h3>Plans</h3>
+                            <p></p>
                             <button onClick={() => auth.signOut()} className="profileScreen__signOut">Sign Out</button>
                         </div>
                         
